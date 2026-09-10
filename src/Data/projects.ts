22d4357ebc094d0aee63d8809a51908cb4f87892
Yaalleee <thrassure135@gmail.com>
2026-09-11 00:56:57 +0700

    update

--- src/Data/projects.ts
+++ src/Data/projects.ts
@@ -10,22 +10,22 @@ export const projects: Project[] = [
   {
     title: "Coffee Lassik",
     desc: "Coffee shop website with modern design and responsive layout.",
-    tags: ["Next.js", "TypeScript", "Tailwind CSS"],
+    tags: ["Next.js", "TypeScript", "Tailwind CSS", "JavaScript"],
     link: "https://coffee-lassik.vercel.app/",
     github: "https://github.com/Yaalleee/CoffeeLassik",
   },
   {
     title: "Satset Ai",
     desc: "satset ai adalah aplikasi berbasis kecerdasan buatan untuk membantu dalam proses pengambilan keputusan.",
-    tags: ["React", "Framer Motion", "Tailwind CSS"],
-    link: "https://satset-ai.netlify.app/",
+    tags: ["TypeScript", "Framer Motion", "Tailwind CSS"],
+    link: "satset-ai.netlify.app//",
     github: "https://github.com/Yaalleee/",
   },
   {
     title: "Listing Kendaraan",
     desc: "Dashboard interaktif untuk visualisasi data penjualan secara real-time.",
     tags: ["Next.js", "Chart.js", "API Integration"],
-    link: "listing-web-kendaraan-syahrial.vercel.app/",
+    link: "https://listing-web-kendaraan-syahrial.vercel.app/",
     github: "https://github.com/Yaalleee/",
   },
 ];
